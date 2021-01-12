6d4b0894efc7f725a45719fad8de5261eafb4839
=Bruno Igor <=brigor7@gmail.com>
2021-01-12 09:54:14 -0300

    Implementação da inclusão de padrinho no BD

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,10 @@ const godfathersController = require('./controller/godfathersController');
 
 const routes = express.Router();
 
-routes.post('/childrens', childrensController.create);
-routes.get('/childrens', childrensController.index);
 routes.post('/godfathers', godfathersController.create);
 routes.get('/godfathers', godfathersController.index);
+routes.post('/childrens', childrensController.create);
+routes.get('/childrens', childrensController.index);
 routes.get('/', childrensController.pesquisa);
 
 module.exports = routes;
--- frontend/src/pages/RegisterGodfather/index.js
+++ frontend/src/pages/RegisterGodfather/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../services/api';
+import { useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import './style.css';
 
 export default function Register() {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [sexo, setSexo] = useState('');
+  const [nascimento, setNascimento] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+  const history = useHistory();
+
+  async function handleRegisterGodfhater(e) {
+    e.preventDefault();
+    const data = {
+      nome,
+      sobrenome,
+      sexo,
+      email,
+      telefone,
+      password,
+      nascimento,
+      avatar,
+    };
+
+    try {
+      await api.post('godfathers', data);
+      alert('Padrinho criado com sucesso');
+      history.push('/');
+    } catch (error) {
+      console.error('Erro ao criar Padrinho: ', error);
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -22,14 +57,47 @@ export default function Register() {
             Retornar para página principal
           </Link>
         </section>
-        <form>
-          <input placeholder="Nome" />
-          <input placeholder="Sobrenome" />
-          <input placeholder="Sexo" />
+        <form onSubmit={handleRegisterGodfhater}>
+          <input
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Nome"
+          />
+          <input
+            value={sobrenome}
+            onChange={(e) => setSobrenome(e.target.value)}
+            placeholder="Sobrenome"
+          />
+          <input
+            value={sexo}
+            onChange={(e) => setSexo(e.target.value)}
+            placeholder="Sexo"
+          />
           <label htmlFor="nascimento">Nascimento</label>
-          <input type="date" placeholder="nascimento" id="nascimento" />
-          <input type="email" placeholder="Email" />
-          <input placeholder="wathsapp" />
+          <input
+            type="date"
+            value={nascimento}
+            onChange={(e) => setNascimento(e.target.value)}
+            placeholder="nascimento"
+            id="nascimento"
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            value={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+            placeholder="telefone"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
           <label htmlFor="avatar">Avatar</label>
           <input type="file" name="avatar" id="avatar" />
 
